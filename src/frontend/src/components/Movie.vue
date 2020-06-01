621e58c1f8f151da9d8488a348ba739ea31ee4d3
tmzkt21 <ehswn67@naver.com>
2020-06-01 18:37:17 +0900

    git

---
+++ src/frontend/src/components/Movie.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h3>검색결과 : {{count}}</h3>
+        <v-simple-table>
+            <template v-slot:default>
+                <thead>
+                <tr>
+
+                    <th class="text-left">No.</th>
+                    <th class="text-left">순위</th>
+                    <th class="text-left">영화제목</th>
+                    <th class="text-left">집계일</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="item of navermovie" :key="item.seq">
+                    <td>{{item.movieSeq}}</td>
+                    <td>{{item.rank}}</td>
+                    <td>{{item.title}}</td>
+                    <td>{{item.rankDate}}</td>
+                </tr>
+                </tbody>
+            </template>
+        </v-simple-table>
+        <div class="text-center">
+            <v-pagination v-model="page" :length="5" :total-visible="5"></v-pagination>
+        </div>
+    </div>
+
+
+</template>
+
+<script>
+    import { mapState } from "vuex";
+    export default {
+        data () {
+            return {
+                page: 1
+            }
+        },
+        computed: {
+            ...mapState({
+                count: state => state.crawling.count,
+                bugsmusic: state => state.crawling.bugsmusic,
+                navermovie:state => state.crawling.navermovie
+
+            })
+        }
+    };
+</script>
